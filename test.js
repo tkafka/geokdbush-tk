9720e23384fff5dfccf6d96475db21d082bbd0ce
Tomáš Kafka <tk@tomaskafka.com>
2023-12-06 12:46:35 +0100

    Test for wrapping around the east/west hemisphere

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 'use strict'
 
 import { test } from 'tape'
+import haversine from 'haversine-distance'
 import cities from 'all-the-cities'
 import KDBush from 'kdbush'
+
 import * as geokdbush from './index.js'
 
 // create
@@ -20,6 +22,57 @@ test('performs search according to maxResults', function (t) {
 	const pointIds = geokdbush.around(index, -119.7051, 34.4363, 5)
 
 	t.same(pointIds.map((id) => cities[id].name).join(', '), 'Mission Canyon, Santa Barbara, Montecito, Summerland, Goleta')
+
+	t.end()
+})
+
+test('search works across the 180 deg boundary', function (t) {
+	function placeDistanceM(origin, place) {
+		let distanceMeters = haversine({ lat: origin[0], lon: origin[1] }, { lat: place.loc.coordinates[1], lon: place.loc.coordinates[0] })
+		// return distance in km rounded to 100 m
+		return `${Math.round(distanceMeters / 100) / 10} km`
+	}
+
+	let placesToCheck = 10
+
+	/// this point is on the east hempsphere, nearest place should be Konergino on the west hemispehere
+	/// lat, lon
+	let egwekinotMid = [66.4392631187067, 180.0]
+	let egwekinotEastHemi = [66.4392631187067, 179.99]
+	let egwekinotWestHemi = [66.4392631187067, -179.99]
+
+	/// lon, lat
+	const pointIdsEast = geokdbush.around(index, egwekinotEastHemi[1], egwekinotEastHemi[0], placesToCheck)
+	const pointIdsWest = geokdbush.around(index, egwekinotWestHemi[1], egwekinotEastHemi[0], placesToCheck)
+
+	const pointIdsEastSet = new Set(pointIdsEast)
+	const pointIdsWestSet = new Set(pointIdsWest)
+
+	t.same(pointIdsEastSet.size, placesToCheck, `Should return ${placesToCheck} places`)
+	t.same(pointIdsWestSet.size, placesToCheck, `Should return ${placesToCheck} places`)
+
+	/// `pointIdsEastSet.intersection(pointIdsWestSet)` is experimental
+	/// replacement from https://exploringjs.com/impatient-js/ch_sets.html#missing-set-operations
+	const intersection = new Set(Array.from(pointIdsEastSet).filter((x) => pointIdsWestSet.has(x)))
+	t.same(intersection.size, placesToCheck, `Intersection should also have ${placesToCheck} as it should be the same places`)
+
+	t.same(pointIdsEast.join('+'), pointIdsWest.join('+'), 'Should return same places')
+
+	const pointsEast = pointIdsEast.map((id) => cities[id])
+	t.ok(
+		pointsEast.some((point) => point.loc.coordinates[0] < 0),
+		'there should be some place in a western hemisphere for easter point',
+	)
+
+	// and vice versa
+	const pointsWest = pointIdsWest.map((id) => cities[id])
+	t.ok(
+		pointsWest.some((point) => point.loc.coordinates[0] > 0),
+		'there should be some place in a eastern hemisphere for western point',
+	)
+
+	console.log('Places east:', pointsEast.map((point) => `${point.name} (${placeDistanceM(egwekinotEastHemi, point)})`).join(', '))
+	console.log('Places west:', pointsWest.map((point) => `${point.name} (${placeDistanceM(egwekinotWestHemi, point)})`).join(', '))
 
 	t.end()
 })
